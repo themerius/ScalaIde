6e8ad4704059acb2311f0e1309e0057e74403811
Sven Hodapp <sven.hodapp@gmail.com>
2012-05-07 15:01:48 +0200

    terminal: now send with return-key

--- public/javascripts/terminal.js
+++ public/javascripts/terminal.js
@@ -12,13 +12,11 @@ IDE.htwg.Terminal = function($){
   };
 
   this.handleKey = function(e) {
-    if(e.charCode == 36 || e.keyCode == 36) {  // hit $ to send
+    if(e.charCode == 13 || e.keyCode == 13) {  // hit return to send
       var msg = {
         "type": "terminal",
         "command": "command",
-        "value": "ls",  // TODO: retrive text from terminal-input
-        "file": "",  // REFACTOR -> not needed
-        "folder": false  // REFACTOR -> not needed
+        "value": "ls"  // TODO: retrive text from terminal-input
       };
       IDE.htwg.websocket.sendMessage(msg);
     }
