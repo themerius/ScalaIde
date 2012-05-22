ddcd2982af6041e1711a9c33a38900761729cfdd
Sven Hodapp <sven.hodapp@gmail.com>
2012-05-22 10:01:10 +0200

    terminal: websocket can now write concurrent.

--- app/models/Terminal.scala
+++ app/models/Terminal.scala
@@ -4,14 +4,13 @@ import scala.sys.process._
 
 import play.api.libs.json._
 
-
 object Terminal {
-
-  // TODO: Refactor: make concurrent (multiuser)
-  // TODO: Need to somehow find out by itself which output channel to use.
 
   var input: java.io.OutputStream = _
   var deactivated = false
+
+  val system = ActorSystem("SendToWebsocket")
+  val websocket = system.actorOf(Props[SendToWebsocket])
 
   def start = {
     if (System.getProperty("os.name").startsWith("Windows")) {
@@ -29,17 +28,17 @@ object Terminal {
 
   def stdout(out: java.io.InputStream) = {
     val lines = scala.io.Source.fromInputStream(out).getLines
-    def inner(str: String) = sendToWebsocket("stdout: " + str)
+    def inner(str: String) = sendToWebsocket("    " + str)
     lines.foreach(inner)
   }
 
   def stderr(out: java.io.InputStream) = {
     val lines = scala.io.Source.fromInputStream(out).getLines
-    def inner(str: String) = println(str)// TODO sendToWebsocket("stderr: " + str)
+    def inner(str: String) = sendToWebsocket(str)
     lines.foreach(inner)
   }
 
-  def sendToWebsocket(output: String) = {
+  def sendToWebsocket(output: String) = this.synchronized {
     val msg = JsObject( Seq(
         "type" -> JsString("terminal"),
         "command" -> JsString("response"),
@@ -55,13 +54,6 @@ object Terminal {
     } else {
       input.write(receivedKey)
       input.flush()
-      /*receivedKey match {
-        case 13 => {
-          input.write(receivedKey)
-          input.flush()
-        }
-        case _ => input.write(receivedKey)
-      }*/
     }
   }
 
