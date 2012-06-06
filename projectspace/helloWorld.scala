6d0561d88916b04f5eb7ac623b96f3c62fc6211e
laeckerv <laeckerv@htwg-konstanz.de>
2012-06-06 15:18:48 +0200

    Projectfiles edited

--- projectspace/helloWorld.scala
+++ projectspace/helloWorld.scala
@@ -1,5 +1,14 @@
+import xtest._
+
 object HelloWorld {
   def main(args: Array[String]) {
     println("Hello, world!")
+    
+    val test = new HelloWorldTest()
+    
+    test.say()
+     
+    val string = "hi"
+    
   }
 }
